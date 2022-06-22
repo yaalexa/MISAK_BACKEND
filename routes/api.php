357176c0a7721406f4ccf026e63b4e274de9cf33
urbano4725 <efurbano@misena.edu.co>
2022-06-22 14:39:32 -0500

    atenticacion de backend crud materials y roles

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,8 @@ use App\Http\Controllers\Material_EducationalController;
 use App\Http\Controllers\Reporters_inicialController;
 use JasperPHP\JasperPHP as JasperPHP;
 use App\Http\Controllers\ReportController;
+use Laravel\Sanctum\Sanctum;
+
 //use App\Http\Controllers\AuthorController;
 
 /*
@@ -40,11 +42,11 @@ Route::put('areas/{id}',[AreaController::class,'update']); // con esta ruta pued
 Route::delete('areas/{id}',[AreaController::class,'destroy']); // con esta ruta puedo eliminar un area
 
 //rutas de author
-Route::get('authors',[AuthorController::class,'index']); // con esta ruta puedo ver todos los autores  
+Route::get('authors',[AuthorController::class,'index']); // con esta ruta puedo ver todos los autores
 Route::post('authors',[AuthorController::class,'store']); //con esta ruta puedo registrar una nuevo autor
 Route::get('authors/{id}',[AuthorController::class,'show']); // con esta ruta puedo buscar un author especifico
 Route::put('authors/{id}',[AuthorController::class,'update']); // con esta ruta puedo actualizar un autor
-Route::delete('authors/{id}',[AuthorController::class,'destroy']); // con esta ruta puedo eliminar un autor      
+Route::delete('authors/{id}',[AuthorController::class,'destroy']); // con esta ruta puedo eliminar un autor
 
 //rutas de User
 Route::post('register', [UserController::class, 'register']);
@@ -58,7 +60,7 @@ Route::get('users',[UserController::class,'index']); // con esta ruta puedo ver 
 Route::post('users',[UserController::class,'store']); //con esta ruta puedo registrar una nuevo usuario
 Route::get('users/{id}',[UserController::class,'show']); // con esta ruta puedo buscar un usuario especifico
 Route::put('users/{id}',[UserController::class,'update']); // con esta ruta puedo actualizar un usuario
-Route::delete('users/{id}',[UserController::class,'destroy']); // con esta ruta puedo eliminar un usuario 
+Route::delete('users/{id}',[UserController::class,'destroy']); // con esta ruta puedo eliminar un usuario
 
 
 //de esta forma nos genera todas las rutas
@@ -70,10 +72,10 @@ Route::put('editorials/{id}',[EditorialController::class,'update']); // con esta
 Route::delete('editorials/{id}',[EditorialController::class,'destroy']); // con esta ruta puedo eliminar una editorial
 
 //rutas de Nivel de educacion
-Route::get('educational_levels',[Educational_LevelController::class,'index']); 
-Route::post('educational_levels',[Educational_LevelController::class,'store']); 
-Route::get('educational_levels/{id}',[Educational_LevelController::class,'show']); 
-Route::put('educational_levels/{id}',[Educational_LevelController::class,'update']); 
+Route::get('educational_levels',[Educational_LevelController::class,'index']);
+Route::post('educational_levels',[Educational_LevelController::class,'store']);
+Route::get('educational_levels/{id}',[Educational_LevelController::class,'show']);
+Route::put('educational_levels/{id}',[Educational_LevelController::class,'update']);
 Route::delete('educational_levels/{id}',[Educational_LevelController::class,'destroy']);
 
 //rutas de tipos de material
@@ -85,11 +87,13 @@ Route::delete('type_materials/{id}',[Type_MaterialController::class,'destroy']);
 // Route::Apiresource('type_materials',[Type_MaterialController::class]);
 
 //rutas de tipos de rol
-Route::get('rols',[RolController::class,'index']);
-Route::get('rols/{id}',[RolController::class,'show']);
-Route::post('rols',[RolController::class,'store']);
-Route::put('rols/{id}',[RolController::class,'update']);
-Route::delete('rols/{id}',[RolController::class,'destroy']);
+Route::group( ['middleware' => ["auth:sanctum"]], function(){
+    Route::get('rols',[RolController::class,'index']);
+    Route::get('rols/{id}',[RolController::class,'show']);
+    Route::post('rols',[RolController::class,'store']);
+    Route::put('rols/{id}',[RolController::class,'update']);
+    Route::delete('rols/{id}',[RolController::class,'destroy']);
+});
 
 //rutas de tipos de autor material
 Route::get('author_materials',[Author_MaterialController::class,'index']);
@@ -100,14 +104,14 @@ Route::delete('author_materials/{id}',[Author_MaterialController::class,'destroy
 
 //rutas de material
 Route::get('/materials',[MaterialController::class,'index']);
-Route::get('/materials/{id}',[MaterialController::class,'show']);
+Route::get('/materials/{id}',[MaterialController::class,'show'])->middleware("auth:sanctum");
 Route::get('/buscar/{id}',[MaterialController::class,'buscar']);
 Route::post('/materials',[MaterialController::class,'store']);
-Route::put('/materials/{id}',[MaterialController::class,'update']);
-Route::delete('/materials/{id}',[MaterialController::class,'destroy']);
-Route::get('/download/{id}',[MaterialController::class,'download']);
+Route::put('/materials/{id}',[MaterialController::class,'update'])->middleware("auth:sanctum");
+Route::delete('/materials/{id}',[MaterialController::class,'destroy'])->middleware("auth:sanctum");
+Route::get('/download/{id}',[MaterialController::class,'download'])/* ->middleware("auth:sanctum") */;
 Route::get('/search/{name}',[MaterialController::class,'buscarm']);
-Route::get('visualizacion',[MaterialController::class,'visualizacion']);
+Route::get('visualizacion',[MaterialController::class,'visualizacion'])->middleware("auth:sanctum");
 Route::get('buscarvisualizacion/{id}',[MaterialController::class,'buscarvisualizacion']);
 //rutas de material_user
 Route::get('material__users',[Material_UserController::class,'index']);
